3c8957073bfd59eb13a788be0583fef9055614a7
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2026-02-25 21:51:26 -0300

    Back escencial terminado

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const planetsRouter = require("./src/routes/planets.routes");
+const morgan = require("morgan");
 const app = express();
 
 //Middleware para poder leer JSON del body
 
 app.use(express.json());
-
+app.use(morgan())
 //Ruta principal
 app.use("/planets", planetsRouter);
 
